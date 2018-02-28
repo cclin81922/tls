a9656acfb45a3c1c4eda645973c9a0c9e90f23cb
Jim Lin <jimlintw922@gmail.com>
2018-02-28 16:04:34 +0800

    add data/monit/httpd

--- client-cert-auth/plug-monit.sh
+++ client-cert-auth/plug-monit.sh
@@ -10,6 +10,7 @@ yum -y install monit-5.25.1-1.el7
 
 # copy configuration files from data dir
 cp /vagrant/data/monit/monitrc /etc/monitrc
+cp /vagrant/data/monit/httpd /etc/monit.d/
 
 # start monit daemon
 systemctl start monit # use port 2812
