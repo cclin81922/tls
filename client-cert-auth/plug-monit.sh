db20d5755a8e76551d400ae70bc6a819a783d8a5
Jim Lin <jimlintw922@gmail.com>
2018-02-28 15:48:15 +0800

    add plug-monit.sh

---
+++ client-cert-auth/plug-monit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+###
+
+# install monit
+yum -y install monit-5.25.1-1.el7
+
+# copy configuration files from data dir
+cp /vagrant/data/monit/monitrc /etc/monitrc
+
+# start monit daemon
+systemctl start monit # use port 2812
